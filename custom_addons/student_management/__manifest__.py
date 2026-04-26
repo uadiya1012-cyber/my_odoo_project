13fd89d2c075e3e4442a35722208d4fca01f2d97
Adiyakhuu Uuriintsolmon <uadiya1012@gmail.com>
2026-04-26 10:43:59 +0800

    add student management app

---
+++ custom_addons/student_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Student Management',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
